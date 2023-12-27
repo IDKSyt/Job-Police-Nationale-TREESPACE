410496b3b8963f49a4a4ef4122e36d55fcce10ab
USER LEAK'S <147881102+IDKSyt@users.noreply.github.com>
2023-12-27 14:18:45 +0100

    Add files via upload

---
+++ TS_Policenatiomap/stream/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'T1JNES'
+
+this_is_a_map 'yes'
+
+
+data_file 'TIMECYCLEMOD_FILE' 'T1JNES_MRPD_TIMECYCLE.xml'
+
+files {
+	'T1JNES_MRPD_TIMECYCLE.xml'
+}
