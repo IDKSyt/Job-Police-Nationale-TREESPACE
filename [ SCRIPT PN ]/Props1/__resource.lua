b1e4c67eafcba3b9d826e02d5d95cdc3a5684039
USER LEAK'S <147881102+IDKSyt@users.noreply.github.com>
2023-12-27 13:54:59 +0100

    Add files via upload

---
+++ [ SCRIPT PN ]/Props1/__resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+author '8Mshop'
+description 'Gendarmerie Nationale'
+
+-- By 8M Shop 2021 © All Rights Reserved FNL --
+
+-- ################### --  -- ################### --  -- ################### --  -- ################### --
+-- ## 8MSHOP FNL GN ## --  -- ## 8MSHOP FNL GN ## --  -- ## 8MSHOP FNL GN ## --  -- ## 8MSHOP FNL GN ## --
+-- ################### --  -- ################### --  -- ################### --  -- ################### --
